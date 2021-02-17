82ef4eb2a466acbe183af7705e081757809e4cc3
Arun Rajput <ar1445942@gmail.com>
2021-02-17 17:24:09 +0530

    initial files add

---
+++ src/components/App.jsx
@@ -0,0 +1,58 @@
+import React, { useState,useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+import axios from "axios";
+
+
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  // const fetchNotes = async () => {
+  //   const {data,status}= await axios.get("https://jsonbox.io/box_3a9ede56cba3aa69c3a4/reactdbs");
+  //   if (status===200){
+  //     setNotes(data);
+  //   };
+  // }
+  
+  // useEffect{() =>{
+  //   fetchNotes();
+  // },[]};
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
